100493a696ef2f1580fc929c7cfedbd486c0c279
Darksied95 <aliyuefe9501@gmail.com>
2023-04-07 18:34:15 +0100

    updated keys naming & added paystack key

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const Router = express.Router()
-const { createUser, deleteUsers, loginUser, userDeposit, transferFund, getUser, withdrawFund } = require('../controllers/user.controller')
+const { createUser,
+    deleteUsers,
+    loginUser,
+    userDeposit,
+    transferFund,
+    getUser,
+    withdrawFund } = require('../controllers/user.controller')
 const auth = require('../middlewares/auth')
 
+Router.get("/me", auth, getUser)
 Router.post('/register', createUser)
 Router.post('/login', loginUser)
-Router.delete("/", deleteUsers)
-Router.get("/me", auth, getUser)
 Router.post('/me/deposit', auth, userDeposit)
 Router.post("/me/transfer", auth, transferFund)
 Router.post("/me/withdraw", auth, withdrawFund)
+Router.delete("/", deleteUsers)
 
 module.exports = Router
 
